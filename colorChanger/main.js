08ef20f48eeb5e837d23e48b96f92b507de8480a
Zohaib Khan <zohaib.k.dev@gmail.com>
2026-02-06 13:47:48 +0500

    added javascript code & project is completed

---
+++ colorChanger/main.js
@@ -0,0 +1,22 @@
+let buttons = document.querySelectorAll(".button");
+let body= document.querySelector("body");
+
+buttons.forEach((button) =>{
+    button.addEventListener("click", (e) => {
+        if(e.target.id === "grey"){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === "white"){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id
+            body.style.color = "white"
+        }
+        if(e.target.id === "yellow"){
+            body.style.backgroundColor = e.target.id
+            body.style.color = "black"
+        }
+        
+    })    
+})
